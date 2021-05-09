6cb3f8271bd2dfbb07acc149377f8db4a2b10563
Bembi <bembi@archerninecloud.onmicrosoft.com>
2021-05-10 02:34:05 +0700

    containerized sample-app

---
+++ challenge-2/sample-app/push-to-registry.sh
@@ -0,0 +1,6 @@
+. .env.local
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker build .
+docker tag $DOCKER_REPO/tvlk-sample-app $DOCKER_REPO/tvlk-sample-app:0.1.0
+docker push $DOCKER_REPO/tvlk-sample-app
+docker push $DOCKER_REPO/tvlk-sample-app:0.1.0
